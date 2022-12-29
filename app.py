2fd8028df2f8b38c04951466eae8f357e8ae50ea
Arjun Niranjan <99181195+ArjunNiranjan1@users.noreply.github.com>
2022-12-29 17:22:13 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
+port = int(os.environ.get('PORT', 5000))
 
 @app.route('/', methods=['GET'])
 def home():
     return render_template("landing.html")
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=port)
