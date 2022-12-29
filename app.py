06a878c76722f6c37ca39703a2ce747ee4736852
Arjun Niranjan <arjunniranjanemail@gmail.com>
2022-12-29 17:45:32 +0000

    unspecify port

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from flask import Flask, render_template
-import os
 
 app = Flask(__name__)
-port = int(os.environ.get('PORT', 5000))
 
 @app.route('/', methods=['GET'])
 def home():
     return render_template("landing.html")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=port)
+    app.run()
