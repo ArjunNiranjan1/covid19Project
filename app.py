495ad9a8fcbc9a0f145254e3548ad1f7bc0852c0
Arjun Niranjan <arjunniranjanemail@gmail.com>
2022-12-31 20:39:47 +0000

    comet

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
 from flask import Flask, render_template
 from sqlalchemy import create_engine
-from application.casesImage import save_to, plot
+from app.casesImage import save_to, plot
 
 app = Flask(__name__)
 
 clouddb = 'cockroachdb://Arjun:ZTP0gR_dTDFPapT53EySTw@chief-wallaby-3959.6zw.cockroachlabs.cloud:26257/covid19-project?sslmode=verify-full'
-engine = create_engine(clouddb)
+
+try:
+    engine = create_engine(clouddb)
+    status = "good"
+except:
+    status = "bad"
 
 t1 = save_to
 plot()
 
 @app.route('/')
 def home():
-    return render_template("landing.html", t1 = t1)
+    return render_template("landing.html", t1 = t1, status = status)
 
 if __name__ == '__main__':
     app.run()
