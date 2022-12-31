229f3b2cef7f712ab43c57b0ad339f5ff7b2262d
Arjun Niranjan <arjunniranjanemail@gmail.com>
2022-12-31 21:21:16 +0000

    t0

--- app.py
+++ app.py
@@ -3,13 +3,15 @@ from application.casesImage import read, plot, save_to
 
 app = Flask(__name__)
 
+t0 = round((9*8)/3,2)
+
 t1 = save_to
 c = read()
 plot()
 
 @app.route('/')
 def home():
-    return render_template("landing.html", t1 = t1, t2 = len(c))
+    return render_template("landing.html", t0 = t0, t1 = t1, t2 = len(c))
 
 if __name__ == '__main__':
     app.run()
