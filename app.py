59c506f353e280092b3b431815f125a8f0dcfa08
Arjun Niranjan <arjunniranjanemail@gmail.com>
2022-12-31 19:48:39 +0000

    import/query tests

--- app.py
+++ app.py
@@ -3,9 +3,12 @@ import application.casesImage
 
 app = Flask(__name__)
 
+out = application.casesImage.save_to
+out2 = len(application.casesImage.cases)
+
 @app.route('/')
 def home():
-    return render_template("landing.html")
+    return render_template("landing.html", content = [out,out2])
 
 if __name__ == '__main__':
     app.run()
