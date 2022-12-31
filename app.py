5ad2716fb993d312a8adc50e37eea52e3af51ef7
Arjun Niranjan <arjunniranjanemail@gmail.com>
2022-12-31 20:02:46 +0000

    undos

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
 from flask import Flask, render_template
-import application.casesImage
 
 app = Flask(__name__)
 
-out = application.casesImage.save_to
-out2 = len(application.casesImage.cases)
-
 @app.route('/')
 def home():
-    return render_template("landing.html", content = [out,out2])
+    return render_template("landing.html")
 
 if __name__ == '__main__':
     app.run()
