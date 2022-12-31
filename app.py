e8aa86f7511e91ff644167e4cf9069f7b3030b5a
Arjun Niranjan <arjunniranjanemail@gmail.com>
2022-12-31 20:49:42 +0000

    back

--- app.py
+++ app.py
@@ -1,26 +1,10 @@
 from flask import Flask, render_template
-from sqlalchemy import create_engine
-from app.casesImage import save_to, plot
 
 app = Flask(__name__)
 
-'''
-clouddb = 'cockroachdb://Arjun:ZTP0gR_dTDFPapT53EySTw@chief-wallaby-3959.6zw.cockroachlabs.cloud:26257/covid19-project?sslmode=verify-full'
-
-try:
-    engine = create_engine(clouddb)
-    status = "good"
-except:
-    status = "bad"
-'''
-t1 = save_to
-
-#plot()
-
-status = "hm"
 @app.route('/')
 def home():
-    return render_template("landing.html", t1 = t1, status = status)
+    return render_template("landing.html")
 
 if __name__ == '__main__':
     app.run()
