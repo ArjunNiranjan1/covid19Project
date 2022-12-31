248211d94506260b81175a81ff653f193236ca67
Arjun Niranjan <arjunniranjanemail@gmail.com>
2022-12-31 20:22:17 +0000

    comet

--- application/casesImage.py
+++ application/casesImage.py
@@ -8,16 +8,19 @@ sns.set_theme()
 save_to = '/application/static/images'
 
 clouddb = 'cockroachdb://Arjun:ZTP0gR_dTDFPapT53EySTw@chief-wallaby-3959.6zw.cockroachlabs.cloud:26257/covid19-project?sslmode=verify-full'
-engine = create_engine(clouddb, connect_args={'ca': '/root.crt'})
 
-query = 'SELECT date, dailycases FROM cases;'
-cases = pd.read_sql_query(query,engine)
-cases = cases.fillna(0)
+try:
+    engine = create_engine(clouddb)
 
-plt.plot(cases["date"],cases["dailycases"],color='purple')
-plt.title("COVID-19 cases in England")
-plt.xlabel("Date")
-plt.ylabel("Cases")
-plt.xticks(cases["date"][list(range(0,len(cases),int(round(len(cases)/5,0))))])
-plt.savefig(save_to + 'cases.png')
-plt.show()
+    query = 'SELECT date, dailycases FROM cases;'
+    cases = pd.read_sql_query(query,engine)
+    cases = cases.fillna(0)
+
+    plt.plot(cases["date"],cases["dailycases"],color='purple')
+    plt.title("COVID-19 cases in England")
+    plt.xlabel("Date")
+    plt.ylabel("Cases")
+    plt.xticks(cases["date"][list(range(0,len(cases),int(round(len(cases)/5,0))))])
+    plt.savefig(save_to + 'cases.png')
+except:
+    cases = "failed to connect to db"
