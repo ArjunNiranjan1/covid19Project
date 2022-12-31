17633c9bf43906d1fd0d934e1cd7e47756d4e431
Arjun Niranjan <arjunniranjanemail@gmail.com>
2022-12-31 20:35:29 +0000

    comet

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 from flask import Flask, render_template
-import application.casesImage
+from sqlalchemy import create_engine
+from application.casesImage import save_to, plot
 
 app = Flask(__name__)
-t1 = application.casesImage.save_to
-t2 = len(application.casesImage.cases)
+
+clouddb = 'cockroachdb://Arjun:ZTP0gR_dTDFPapT53EySTw@chief-wallaby-3959.6zw.cockroachlabs.cloud:26257/covid19-project?sslmode=verify-full'
+engine = create_engine(clouddb)
+
+t1 = save_to
+plot()
 
 @app.route('/')
 def home():
-    return render_template("landing.html", t1 = t1, t2 = t2)
+    return render_template("landing.html", t1 = t1)
 
 if __name__ == '__main__':
     app.run()
--- application/casesImage.py
+++ application/casesImage.py
@@ -3,15 +3,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-sns.set_theme()
-
 save_to = '/application/static/images'
 
 clouddb = 'cockroachdb://Arjun:ZTP0gR_dTDFPapT53EySTw@chief-wallaby-3959.6zw.cockroachlabs.cloud:26257/covid19-project?sslmode=verify-full'
+engine = create_engine(clouddb)
 
-try:
-    engine = create_engine(clouddb)
-
+def plot():
+    sns.set_theme()
+    
     query = 'SELECT date, dailycases FROM cases;'
     cases = pd.read_sql_query(query,engine)
     cases = cases.fillna(0)
@@ -22,5 +21,3 @@ try:
     plt.ylabel("Cases")
     plt.xticks(cases["date"][list(range(0,len(cases),int(round(len(cases)/5,0))))])
     plt.savefig(save_to + 'cases.png')
-except:
-    cases = "failed to connect to db"
