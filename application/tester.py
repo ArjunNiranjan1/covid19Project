f1f801711d272e13763d5e44f49ba2c7eea1bbcf
Arjun Niranjan <arjunniranjanemail@gmail.com>
2023-01-01 15:14:33 +0000

    find_dotenv

--- application/tester.py
+++ application/tester.py
@@ -2,13 +2,14 @@ from sqlalchemy import create_engine
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 import os
 
 save_to = '/app/application/static/images'
 
 base = os.getcwd()
-load_dotenv(os.path.join(base,'.env'))
+#path = os.path.join(base,'.env')
+load_dotenv(find_dotenv())
 
 user = os.getenv("DB_USER")
 pw = os.getenv("DB_PASSWORD")
