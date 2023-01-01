fbfc548ad6c1101cd905745d6f621eeb3a8aacf1
Arjun Niranjan <arjunniranjanemail@gmail.com>
2023-01-01 13:44:13 +0000

    clean landing page

--- app.py
+++ app.py
@@ -1,19 +1,16 @@
 from flask import Flask, render_template
-from application.casesImage import read, plot, save_to
+from application.casesImage import read, plot
 import os
 
 app = Flask(__name__)
 
-t0 = save_to
-
-t1 = os.getcwd()
-c = read()
+#c = read()
 #plot()
 
 
 @app.route('/')
 def home():
-    return render_template("landing.html", t0 = t0, t1 = t1, t2 = len(c))
+    return render_template("landing.html")
 
 if __name__ == '__main__':
     app.run()
