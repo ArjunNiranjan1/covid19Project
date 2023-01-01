1fa6cc18117df96d991bb9d2927787e0e4f887f8
Arjun Niranjan <arjunniranjanemail@gmail.com>
2023-01-01 10:51:33 +0000

    cwd

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
 from flask import Flask, render_template
 from application.casesImage import read, plot, save_to
+import os
 
 app = Flask(__name__)
 
 t0 = round((9*8)/3,2)
 
-t1 = save_to
+t1 = os.getcwd()
 
 #c = read()
 #plot() desktop/arjun/projects
 
+'''
 f = open('/covid19Project/root.crt', 'r')
 c = f.read()
 f.close()
-
+'''
 
 @app.route('/')
 def home():
