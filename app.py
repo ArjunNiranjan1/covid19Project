338f2fba4f2a91acb8f16d4050f6dade2ee69f89
Arjun Niranjan <arjunniranjanemail@gmail.com>
2023-01-01 11:47:15 +0000

    db

--- app.py
+++ app.py
@@ -7,18 +7,13 @@ app = Flask(__name__)
 t0 = save_to
 
 t1 = os.getcwd()
-#c = read()
+c = read()
 #plot()
-
-path = t1 + '/application/root.crt'
-f = open(path, 'r')
-c = f.read()
-f.close()
 
 
 @app.route('/')
 def home():
-    return render_template("landing.html", t0 = t0, t1 = t1, t2 = c)
+    return render_template("landing.html", t0 = t0, t1 = t1, t2 = len(c))
 
 if __name__ == '__main__':
     app.run()
--- application/casesImage.py
+++ application/casesImage.py
@@ -2,11 +2,16 @@ from sqlalchemy import create_engine
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import os
 
 save_to = '/application/static/images'
-
+user = 'Arjun'
+pw = 'ZTP0gR_dTDFPapT53EySTw'
 def read():
-    clouddb = 'cockroachdb://Arjun:ZTP0gR_dTDFPapT53EySTw@chief-wallaby-3959.6zw.cockroachlabs.cloud:26257/covid19-project?sslmode=verify-ca&sslrootcert=/covid19Project/root.crt'
+    user = 'Arjun'
+    pw = 'ZTP0gR_dTDFPapT53EySTw'
+    path = os.getcwd() + '/application/root.crt'
+    clouddb = f'cockroachdb://{user}:{pw}@chief-wallaby-3959.6zw.cockroachlabs.cloud:26257/covid19-project?sslmode=verify-ca&sslrootcert={path}'
     engine = create_engine(clouddb)
     query = 'SELECT date, dailycases FROM cases;'
     cases = pd.read_sql_query(query,engine)
