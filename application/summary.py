dfbb143a635493a2d32127087bdcaa5813869ccb
Arjun Niranjan <arjunniranjanemail@gmail.com>
2023-01-02 13:40:01 +0000

    landing update

---
+++ application/summary.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+
+base = os.getcwd()
+user = os.getenv('DB_USER')
+pw = os.getenv("DB_PASSWORD")
+
+#Connect to db
+
+#Query date, daily cases, daily deaths
+
+#Generate plots
+
