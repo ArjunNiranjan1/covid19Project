fcc9f70d2f5c1402f2861b39601661c990a49bcf
Arjun Niranjan <arjunniranjanemail@gmail.com>
2023-01-03 13:30:15 +0000

    comments

--- app.py
+++ app.py
@@ -4,15 +4,21 @@ from application.summary import connect, get_df, cases, deaths, mortality, full_
 app = Flask(__name__)
 
 #Summary plots
+#Summary data
 summary_data = get_df(connect())
+#Cases time series and path string for html
 c = cases(summary_data)
 cases_time_series = f'data:image/png;base64,{c}'
+#Deaths time series and path string for html
 d = deaths(summary_data)
 deaths_time_series = f'data:image/png;base64,{d}'
+#Mortality time series and path string for html
 m = mortality(summary_data)
 mort_plot = f'data:image/png;base64,{m}'
+#Histogram of daily death count (full)
 f = full_hist(summary_data)
 full_plot = f'data:image/png;base64,{f}'
+#Histograms of daily death counts (split)
 s = split_hist(summary_data)
 split_plot = f'data:image/png;base64,{s}'
 
