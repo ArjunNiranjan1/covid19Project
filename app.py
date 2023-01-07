e4669fcebc3fab6ea190fff7c6a32702a237c9b2
Arjun Niranjan <arjunniranjanemail@gmail.com>
2023-01-07 13:24:55 +0000

    about page

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def home():
     return render_template("landing.html", data_dict = data_dict, plot_dict = plot_dict)
 
+@app.route('/about/')
+def about():
+    return render_template('about.html')
+
 @app.route('/features/')
 def features():
     return render_template('features.html')
